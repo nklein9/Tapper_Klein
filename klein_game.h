36b84ecb0cbb273878edde91f94b30a968748318
Nick Klein <nklein9e@gmail.com>
2019-03-25 15:36:18 -0400

    uploading project to github

---
+++ klein_game.h
@@ -0,0 +1,31 @@
+/*
+ *
+ * "$KLEIN play" will start the game.
+ * "$KLEIN help" will print information about the game.
+ *
+ * Enjoy!
+ *
+ * @warning This game runs best at 16MHz or higher. See @ref hal_clock
+ *
+ *  Created on: Mar 18, 2018
+ *      Author: Nicholas Klein
+ *  Edited on: Mar 24, 2018
+ *      Author: Nicholas Klein
+ *  Created using muh_game.h as a template
+ *  Created on: Mar 29, 2014
+ *      Author: Muhlbaier
+ *
+ *  @{
+ */
+
+#ifndef KLEIN_GAME_H_
+#define KLEIN_GAME_H_
+
+/** KLEIN_Init must be called before the game can be played
+ *
+ */
+void KleinGame_Init(void);
+
+/** @} */
+
+#endif /* KLEIN_GAME_H_ */
